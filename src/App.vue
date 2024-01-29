97f947d60c45377f5a28d3602b56c006513adff8
Richiisontheway <mineiriccardo@gmail.com>
2024-01-29 16:27:52 +0100

    general commit

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
-import axios from 'axios';
+import {store} from './store';
 export default {
     data() {
         return {
-
+            store
         };
     },
     components: {
@@ -18,17 +18,12 @@ export default {
 
     },
     mounted(){
-        axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0').then((response)=>{
-            console.log(response)
-        })
+        
     }
 }
 </script>
 
 <template>
-    <h1>
-        Mia App
-    </h1>
 
     <AppHeader />
 
